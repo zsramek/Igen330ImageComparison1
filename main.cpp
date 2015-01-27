b4d522fff33f8e9c1a66d25964438d59547becb3
Zefan <zefan@SwagNugget.swag.ca>
2015-01-27 15:16:52 -0800

    Added Sobel edge detection. Need to add comparison function for grayscale images.

--- main.cpp
+++ main.cpp
@@ -3,19 +3,25 @@
 #include <stdlib.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 using namespace std;
 using namespace cv;
 
 void compareImages (Mat& img1, Mat& img2);
+void sobelImages (Mat& img1, Mat& img2, Mat& img1Sobel, Mat& img2Sobel);
 
 int main()
 {
     Mat img1;
     Mat img2;
+    Mat img1Sobel;
+    Mat img2Sobel;
 
     img1 = imread("/home/zefan/Documents/toTest/blank.png", CV_LOAD_IMAGE_UNCHANGED);
     img2 = imread("/home/zefan/Documents/toTest/test.png", CV_LOAD_IMAGE_UNCHANGED);
+    img1Sobel = imread("/home/zefan/Documents/toTest/blank.png", CV_LOAD_IMAGE_UNCHANGED);
+    img2Sobel = imread("/home/zefan/Documents/toTest/test.png", CV_LOAD_IMAGE_UNCHANGED);
 
     if(img1.empty())
     {
@@ -46,9 +52,16 @@ int main()
     }
     while(i != 1);
 
-    compareImages(img1, img2);
+    sobelImages(img1, img2, img1Sobel, img2Sobel);
 
-    imshow("Comparison", img2);
+    imshow("Perfect", img1Sobel);
+    imshow("Comparison", img2Sobel);
+
+    waitKey(0);
+
+    compareImages(img1Sobel, img2Sobel);
+
+    imshow("Comparison", img2Sobel);
 
     waitKey(0);
 
@@ -58,8 +71,75 @@ int main()
 
 }
 
+void sobelImages(Mat& img1, Mat& img2, Mat& img1Sobel, Mat& img2Sobel)
+{
+    int scale = 1;
+    int delta = 0;
+    int ddepth = CV_16S;
+
+    Mat img1Gray;
+    Mat img2Gray;
+
+    ///Apply blur to reduce noise
+    GaussianBlur(img1, img1, Size(3,3), 0, 0, BORDER_DEFAULT);
+    GaussianBlur(img2, img2, Size(3,3), 0, 0, BORDER_DEFAULT);
+
+    /// Convert images to gray
+    cvtColor(img1, img1Gray, CV_RGB2GRAY);
+    cvtColor(img2, img2Gray, CV_RGB2GRAY);
+
+    ///++++++++++++++FOR IMG1+++++++++++++++++++++++++++++++++
+
+    /// Generate gradX and gradY
+    Mat gradX1, gradY1;
+    Mat absGradX1, absGradY1;
+
+    /// Gradient X
+    //Scharr( src_gray, grad_x, ddepth, 1, 0, scale, delta, BORDER_DEFAULT );
+    Sobel(img1Gray, gradX1, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT);
+    convertScaleAbs(gradX1, absGradX1);
+
+    /// Gradient Y
+    //Scharr( src_gray, grad_y, ddepth, 0, 1, scale, delta, BORDER_DEFAULT );
+    Sobel(img1Gray, gradY1, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT);
+    convertScaleAbs(gradY1, absGradY1);
+
+    /// Total Gradient (approximate)
+    addWeighted( absGradX1, 0.5, absGradY1, 0.5, 0, img1Sobel);
+
+    ///++++++++++++++FOR IMG2+++++++++++++++++++++++++++++++++
+
+    /// Generate gradX and gradY
+    Mat gradX2, gradY2;
+    Mat absGradX2, absGradY2;
+
+    /// Gradient X
+    //Scharr( src_gray, grad_x, ddepth, 1, 0, scale, delta, BORDER_DEFAULT );
+    Sobel(img2Gray, gradX2, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT);
+    convertScaleAbs(gradX2, absGradX2);
+
+    /// Gradient Y
+    //Scharr( src_gray, grad_y, ddepth, 0, 1, scale, delta, BORDER_DEFAULT );
+    Sobel(img2Gray, gradY2, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT);
+    convertScaleAbs(gradY2, absGradY2);
+
+    /// Total Gradient (approximate)
+    addWeighted( absGradX2, 0.5, absGradY2, 0.5, 0, img2Sobel);
+
+    return;
+}
+
 void compareImages (Mat& img1, Mat& img2)
 {
+
+    /*
+    Mat img1Final;
+    Mat img2Final;
+
+    cvtColor(img1, img1Final, CV_GRAY2RGB);
+    cvtColor(img2, img2Final, CV_GRAY2RGB);
+    */
+
     const int channels = img2.channels();
 
     MatIterator_<Vec3b> it1, end1, it2, end2;
